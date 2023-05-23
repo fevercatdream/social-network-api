ac80033a772685ea7ac4de281eb504bee6b064ad
fevercatdream <kuma18@uw.edu>
2023-05-22 23:00:41 -0700

    added users to data.js and filled in seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,10 +1,24 @@
 const connection = require('../config/connection');
 const { User, Thought, Reaction } = require('../models');
+const { users, thoughts } = require('./data');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
     console.log('connected');
 
-    
+    // drop existing users
+    await User.deleteMany({});
+
+    // drop existing thoughts
+    await Thought.deleteMany({});
+
+    // insert seed users
+    await User.collection.insertMany(users);
+
+    // insert seed thoughts
+    await Thought.collection.insertMany(thoughts);
+
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
 });
